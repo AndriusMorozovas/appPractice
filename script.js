9245a81bf1f105a09e9651eb5257bd9f2c7a9aff
andriusmorozovas <morozovas.andrius@gmail.com>
2024-11-29 10:59:57 +0200

    third commit

--- script.js
+++ script.js
@@ -4,6 +4,35 @@ const container = document.querySelector('.container')
 fetch("https://api.escuelajs.co/api/v1/users")
 .then(res => res.json())
 .then(data => {
-    console.log(data)
+
+    data.map(user => {
+        container.innerHTML += `
+<div class="user-box">
+        <div>${user.id}</div>
+        <img src="${user.avatar}" alt="User Avatar" class="avatar">
+        <h1>${user.name}</h1>
+        <p class="label">Role: </p><p>${user.role}</p>
+        <div class="user-details">
+            <p><span class="label">Email:</span> ${user.email}</p>
+            <p><span class="label">Account Created:</span> <span class="timestamp">${user.creationAt}</span></p>
+            <p><span class="label">Last Updated:</span> <span class="timestamp">${user.updatedAt}</span></p>
+        </div>
+        <button>Delete user</button>
+        </div>`
+
+        })
+
+
 })
 
+const btn = document.querySelectorAll('button')
+
+btn.forEach(item => {
+    btn.onclick = () => {
+        fetch(`https://api.escuelajs.co/api/v1/users/${id}`, {
+            method: 'DELETE'
+        }).then(() => {
+            console.log('removed');
+        })
+    }
+})
